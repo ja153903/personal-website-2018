79bf375c26e5fdfedbac833be44f207686612409
ja153903 <jaimeabbariao@gmail.com>
2018-08-27 00:01:38 -0400

    Implemented database insertion methods

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 
@@ -25,6 +27,22 @@ func handleTextPost(router *gin.Engine) {
 		})
 
 		// at this point, we want to add the post to the table
+		db, err := sql.Open("mysql", "root:password@/blog")
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+		
+		sqlQuery := `
+		INSERT INTO POSTS (blog_post, date_posted)
+		VALUES (?, ?)
+		`
+		_, err = db.Exec(sqlQuery, blogPost.BlogEntry, blogPost.Date)
+		if err != nil {
+			panic(err)
+		}
+
 	})
 }
 
